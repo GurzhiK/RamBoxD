0815cb819c5a61859d7c883aed39d7f05ded4088
GurzhiK <gurzhiy_pro@mail.ru>
2023-09-27 19:31:10 +0300

    third commit

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -6,10 +6,13 @@ User = get_user_model()
 
 
 class UserCreateSerializer(UserCreateSerializer):
+    bio = serializers.CharField(required=False)
+    avatar = serializers.ImageField(required=False)
+
     class Meta(UserCreateSerializer.Meta):
         model = User
         fields = ('id', 'email', 'password', 'first_name',
-                  'last_name', 'phone_number', 'username')
+                  'last_name', 'phone_number', 'username', 'bio', 'avatar')
 
 
 class UserListSerializer(serializers.ModelSerializer):
