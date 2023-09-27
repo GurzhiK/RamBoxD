9445a3508b1de1407bf24c6efb574fd6e74b11ee
GurzhiK <94523039+GurzhiK@users.noreply.github.com>
2023-09-27 18:20:10 +0300

    Update settings.py

--- letterboxd/settings.py
+++ letterboxd/settings.py
@@ -134,7 +134,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'meatmainstore@gmail.com'
-EMAIL_HOST_PASSWORD = 'tyvquefdwwcwjyve'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_USE_TLS = True
 
 REST_FRAMEWORK = {
